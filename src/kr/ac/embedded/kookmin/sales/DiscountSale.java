abb1eb302519f8e784c940c76d04b866cc54cddf
siwonsong <ghki007@kookmin.ac.kr>
2015-10-01 17:39:06 +0900

    2015.10.01 20102006 siwon song _ DiscountSale is adjusted

--- src/kr/ac/embedded/kookmin/sales/DiscountSale.java
+++ src/kr/ac/embedded/kookmin/sales/DiscountSale.java
@@ -8,54 +8,75 @@ package kr.ac.embedded.kookmin.sales;
  */
 
 public class DiscountSale extends Sale {
-	private double	discount;	// A percent of the price. Cannot be negative.
-								
+	private double discount; // A percent of the price. Cannot be negative.
+
 	public DiscountSale() {
-		/** 구현 하시오 **/ 
+		/** 구현 하시오 **/
 	}
-	
+
 	/**
 	 * Precondition: theName is a nonempty string; thePrice is nonnegative;
 	 * theDiscount is expressed as a percent of the price and is nonnegative.
 	 */
 	public DiscountSale(String theName, double thePrice, double theDiscount) {
-		/** 구현 하시오 **/ 
-		super.name = theName;
-		super.price = thePrice;
-		discount = theDiscount;
+		/** 구현 하시오 **/
+		if (theName == null) {
+			System.out.print("Name is empty.");
+			System.exit(0);
+		} else if (thePrice < 0) {
+			System.out.print("Price is negative.");
+			System.exit(0);
+		} else if (theDiscount < 0) {
+			System.out.print("Discount is negative.");
+			System.exit(0);
+		} else {
+			super.name = theName;
+			super.price = thePrice;
+			discount = theDiscount;
+		}
+
 	}
-	
+
 	public DiscountSale(DiscountSale originalObject) {
-		/** 구현 하시오 **/ 
+		/** 구현 하시오 **/
+
 	}
-	
+
 	public static void announcement() {
 		System.out.println("This is the DiscountSale class.");
 	}
-	
+
 	public double bill() {
-		/** 구현 하시오 **/ 
-		return (getPrice()-getPrice()*discount/100);
+		/** 구현 하시오 **/
+		return (getPrice() - getPrice() * discount / 100);
 	}
-	
+
 	public double getDiscount() {
-		/** 구현 하시오 **/ 
+		/** 구현 하시오 **/
 		return discount;
 	}
-	
+
 	/**
 	 * Precondition: Discount is nonnegative.
 	 */
 	public void setDiscount(double newDiscount) {
-		/** 구현 하시오 **/ 
+		/** 구현 하시오 **/
+		if (newDiscount < 0) {
+			System.out.print("Price is negative.");
+			System.exit(0);
+		} else {
+			discount = newDiscount;
+		}
+
 	}
-	
+
 	public String toString() {
-		return (getName() + " Price = $" + getPrice() + " Discount = " + discount + "%\n" + "   Total cost = $" + bill());
+		return (getName() + " Price = $" + getPrice() + " Discount = " + discount + "%\n" + "   Total cost = $"
+				+ bill());
 	}
-	
+
 	public boolean equals(Object otherObject) {
-		/** 구현 하시오 **/ 
+		/** 구현 하시오 **/
 		if (otherObject == null)
 			return false;
 		else if (getClass() != otherObject.getClass())
@@ -65,10 +86,9 @@ public class DiscountSale extends Sale {
 			return (name.equals(otherSale.name) && (price == otherSale.price));
 		}
 	}
-	
-	
+
 	public DiscountSale clone() {
-		/** 구현 하시오....  임시생성자 사용 **/ 
+		/** 구현 하시오.... 임시생성자 사용 **/
 		return new DiscountSale(this);
 	}
 }
