35f87676479cf1de5fc1a3e9d315690e3867d891
siwonsong <ghki007@kookmin.ac.kr>
2015-10-01 16:32:20 +0900

    2015.10.01 송시원

---
+++ src/kr/ac/embedded/kookmin/sales/DiscountSale.java
@@ -0,0 +1,74 @@
+package kr.ac.embedded.kookmin.sales;
+
+/**
+ * Class for a sale of one item with discount expressed as a percent of the
+ * price, but no other adjustments. Class invariant: The price is always
+ * nonnegative; the name is a nonempty string; the discount is always
+ * nonnegative.
+ */
+
+public class DiscountSale extends Sale {
+	private double	discount;	// A percent of the price. Cannot be negative.
+								
+	public DiscountSale() {
+		/** 구현 하시오 **/ 
+	}
+	
+	/**
+	 * Precondition: theName is a nonempty string; thePrice is nonnegative;
+	 * theDiscount is expressed as a percent of the price and is nonnegative.
+	 */
+	public DiscountSale(String theName, double thePrice, double theDiscount) {
+		/** 구현 하시오 **/ 
+		super.name = theName;
+		super.price = thePrice;
+		discount = theDiscount;
+	}
+	
+	public DiscountSale(DiscountSale originalObject) {
+		/** 구현 하시오 **/ 
+	}
+	
+	public static void announcement() {
+		System.out.println("This is the DiscountSale class.");
+	}
+	
+	public double bill() {
+		/** 구현 하시오 **/ 
+		return (getPrice()-getPrice()*discount/100);
+	}
+	
+	public double getDiscount() {
+		/** 구현 하시오 **/ 
+		return discount;
+	}
+	
+	/**
+	 * Precondition: Discount is nonnegative.
+	 */
+	public void setDiscount(double newDiscount) {
+		/** 구현 하시오 **/ 
+	}
+	
+	public String toString() {
+		return (getName() + " Price = $" + getPrice() + " Discount = " + discount + "%\n" + "   Total cost = $" + bill());
+	}
+	
+	public boolean equals(Object otherObject) {
+		/** 구현 하시오 **/ 
+		if (otherObject == null)
+			return false;
+		else if (getClass() != otherObject.getClass())
+			return false;
+		else {
+			Sale otherSale = (Sale) otherObject;
+			return (name.equals(otherSale.name) && (price == otherSale.price));
+		}
+	}
+	
+	
+	public DiscountSale clone() {
+		/** 구현 하시오....  임시생성자 사용 **/ 
+		return new DiscountSale(this);
+	}
+}
